b3b94634608c9aa5151f58b7fd17d998e941bc30
tofuu-x <147022000+tofuu-x@users.noreply.github.com>
2025-01-20 12:16:54 +1100

    hashed the password and created a function that shows the dashboard after authentication in home.html

--- src/Routes/authRoute.js
+++ src/Routes/authRoute.js
@@ -1,18 +1,24 @@
 import { Router } from "express";
+import bcrypt from 'bcryptjs';
 
 const router=Router();
 
-router.post('/login',(req,res)=>{
-  const {username,password}=req.body
-  console.log(username,password)
-  res.json('OKsdfsd')
-})
+//* Save the username and password to the database.
+//Todo: Take the password and hash it. (one way, suitable for securely storing password)(DONE)
+//Todo: Save the username and hashed password to the database.
 
 router.post('/register',(req,res)=>{
   const {username,password}=req.body
-  console.log(username,password)
-  res.json('Logged in')
+  const hashedPassword=bcrypt.hashSync(password,12)
+  console.log(hashedPassword);
+  res.json({token:'successful'})
 })
+
+router.post('/login',(req,res)=>{
+  const {username,password}=req.body
+  res.json('OKsdfsd')
+})
+
 
 
 export default router;
