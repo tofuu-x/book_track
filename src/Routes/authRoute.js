df6c577f87d41f1cae1ab53fd2e2f2dc7f169ab1
tofuu-x <147022000+tofuu-x@users.noreply.github.com>
2025-01-21 11:48:46 +1100

    Fixed styling issues and completed the register feature.

--- src/Routes/authRoute.js
+++ src/Routes/authRoute.js
@@ -1,17 +1,34 @@
 import { Router } from "express";
 import bcrypt from 'bcryptjs';
+import db from '../db.js'
+import jwt from 'jsonwebtoken'
 
 const router=Router();
 
 //* Save the username and password to the database.
+//Todo: See if the username is taken or not
 //Todo: Take the password and hash it. (one way, suitable for securely storing password)(DONE)
 //Todo: Save the username and hashed password to the database.
 
 router.post('/register',(req,res)=>{
   const {username,password}=req.body
-  const hashedPassword=bcrypt.hashSync(password,12)
-  console.log(hashedPassword);
-  res.json({token:'successful'})
+
+  try{
+    const lookupStatement=db.prepare('SELECT * FROM users WHERE username=(?)')
+    const lookupResult=lookupStatement.get(username)
+    if(lookupResult){
+       return res.status(400).json({error:"Username already in use"})
+    }
+    const hashedPassword=bcrypt.hashSync(password,12)
+    const registerStatement=db.prepare(`INSERT INTO users(username,password) VALUES (?,?)`)
+    const insertedUser=registerStatement.run(username,hashedPassword);
+    const token=jwt.sign({id:insertedUser.lastInsertRowid},process.env.JWT_SECRET_KEY,{expiresIn:'1d'})
+    console.log(token)
+    res.json({token:'successful'})
+  }catch(error){
+    console.log(error);
+  }
+  
 })
 
 router.post('/login',(req,res)=>{
